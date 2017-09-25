611161a1a05927b7c4e770b28e9ed49a8de28207
seba316d <sebamanczak@gmail.com>
2017-09-25 13:24:08 +0200

    Zmiany w plikach konfiguracji oraz dodanie htaccess

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'pgsql:host=localhost;dbname=wsb_inz',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'username' => 'sa',
+	'password' => 'Hed*2sxZc',
+	'database' => 'wsb_inz',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
---
+++ application/controllers/Account.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account extends CI_Controller {
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->library('twig');
+        $this->load->model('site/Site_model');
+    }
+
+    public function index()
+    {
+
+    }
+
+
+}
