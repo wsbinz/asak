07bfa46810da77dedcd02686457539e0ced57ce0
seba316d <sebamanczak@gmail.com>
2017-09-27 20:48:24 +0200

    Logowanie + tworzenie usera

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wsb_inz/';
+$config['base_url'] = 'http://ts3-tnt.pl/wsb_inz/';
 
 /*
 |--------------------------------------------------------------------------
