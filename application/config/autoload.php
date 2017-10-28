32da2d1d4962b7b1f01c7f2d1b4583cdffccd7df
seba316d <sebamanczak@gmail.com>
2017-10-28 20:23:17 +0200

    Zmiana nazwy helpera

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','form','cookie','text','My');
+$autoload['helper'] = array('url','file','form','cookie','text','MY_');
 
 /*
 | -------------------------------------------------------------------
