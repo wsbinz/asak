3c0f1ab2abed92ace8b0ff8504dfa1a7ed1acfc3
seba316d <sebamanczak@gmail.com>
2017-10-28 20:03:18 +0200

    zmiana w helpers

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','form','cookie','text','my');
+$autoload['helper'] = array('url','file','form','cookie','text','My');
 
 /*
 | -------------------------------------------------------------------
