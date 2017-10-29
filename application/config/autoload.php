fec3baffb026dcb6f2766a61b15ede2a40fe60f6
seba316d <sebamanczak@gmail.com>
2017-10-29 15:01:33 +0100

    zmiana w autoload nazwy

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','form','cookie','text','MY_');
+$autoload['helper'] = array('url','file','form','cookie','text','my');
 
 /*
 | -------------------------------------------------------------------
