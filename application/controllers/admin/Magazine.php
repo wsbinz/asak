b3b84134fd28698cbd06944c9ed995b8e81d10c3
PizzaLap <adam.sawada@gmail.com>
2017-12-10 18:09:52 +0100

    Poprawienie nazw magazynów na j. angielski - dodanie widoku wszystkich dostępnych magazynów /admin/magazine/view_magazine

--- application/controllers/admin/Magazine.php
+++ application/controllers/admin/Magazine.php
@@ -18,7 +18,9 @@ class Magazine extends Admin_Controller
         $this->load->library('session');
         $this->load->model('admin/Admin_model');
         $this->load->helper('My');
+        $this->load->library('pagination');
     }
+
 
     public function index()
     {
@@ -31,33 +33,33 @@ class Magazine extends Admin_Controller
         if(!empty($_POST))
         {
 
-            $magazyn = $this->input->post('magazyn',true);
-            $NazwaMagazynu = $this->input->post('NazwaMagazynu',true);
+            $magazine = $this->input->post('magazine',true);
+            $NameOfMagazin = $this->input->post('NameOfMagazin',true);
 
-            if(!empty($magazyn))
+            if(!empty($magazine))
             {
-                if(!empty($NazwaMagazynu))
+                if(!empty($NameOfMagazin))
                 {
                 $data = array(
-                    'load_group' => $magazyn,
-                    'load_group_descr' => $NazwaMagazynu,
+                    'load_group' => $magazine,
+                    'load_group_descr' => $NameOfMagazin,
                 );
 
 
                 $this->Admin_model->create("STORAGE", $data);
-                    $this->session->set_flashdata('alert', "Hura dodales magazyn !");
+                    $this->session->set_flashdata('alert', "Dodales magazyn!");
 
                 }
                 else{
                     $this->session->set_flashdata('alert', "Uzupełnij wszystkie pola!");
-                    $data['post_2'] = $magazyn;
+                    $data['post_2'] = $magazine;
                 }
 
             }
             else
             {
                 $this->session->set_flashdata('alert', "Uzupełnij wszystkie pola!");
-                $data['post_1'] = $NazwaMagazynu;
+                $data['post_1'] = $NameOfMagazin;
             }
 
         }
@@ -66,4 +68,49 @@ class Magazine extends Admin_Controller
         $data['validation'] = $this->session->flashdata('alert');
         $this->twig->display('admin/magazine/add_magazine',$data);
     }
+    public function view_magazine()
+    {
+        $total_rows = $this->Admin_model->num_rows("STORAGE");
+        $start_index = ($this->uri->segment(10)) ? $this->uri->segment(10) : 0 ;
+
+        $config['total_rows'] = $total_rows;
+        $config['per_page'] = 10;
+        $config['uri_segment'] = 10;
+        $config['base_url'] = base_url('admin/magazine/view_magazine');
+
+        $config['full_tag_open'] = '<ul class="pagination">';
+        $config['full_tag_close'] = '</ul>';
+
+        $config['first_link'] = 'First';
+        $config['first_tag_open'] = '<li class="">';
+        $config['first_tag_close'] = '</li>';
+
+        $config['last_link'] = 'Last';
+        $config['last_tag_open'] = '<li class="">';
+        $config['last_tag_close'] = '</li>';
+
+        $config['next_link'] = 'Next';
+        $config['next_tag_open'] = '<li class="page-item"><span class="page-link">';
+        $config['next_tag_close'] = '</span></li>';
+
+        $config['prev_link'] = 'Previous';
+        $config['prev_tag_open'] = '<li class="page-item"><span class="page-link">';
+        $config['prev_tag_close'] = '</span></li>';
+
+        $config['cur_tag_open'] = '<li class="page-item active"><a href="">';
+        $config['cur_tag_close'] = '</a></li>';
+
+        $config['num_tag_open'] = '<li class="page-item">';
+        $config['num_tag_close'] = '</li>';
+
+        $data['views'] = $this->Admin_model->get("STORAGE",$config['per_page'],$start_index);
+
+        $this->pagination->initialize($config);
+        $data['links'] = $this->pagination->create_links();
+
+        $data['validation'] = $this->session->flashdata('alert');
+        $this->twig->display('site/magazine/list_magazine',$data);
+
+    }
+
 }
