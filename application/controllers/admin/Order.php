b344f1db5c57791d91bd775b117a5baa2b0d8a0c
Boruś <kamil.borowicz@wp.pl>
2018-01-21 03:42:59 +0100

    Formularz zamówienia wraz z podglądem doc

--- application/controllers/admin/Order.php
+++ application/controllers/admin/Order.php
@@ -20,67 +20,55 @@ class Order extends Admin_Controller
     {
         if(!empty($_POST)) {
 
-            $data = array(
-                'nr_docum' => $this->input->post('nr_docum', true),
-                'docum_type' => $this->input->post('docum_type', true),
-                'vend_name' => $this->input->post('vend_name', true),
-                'docum_desc' => $this->input->post('docum_desc', true),
-            );
-            $this->Admin_model->create("DOCUM_HEAD", $data);
+            $docum_value = $this->input->post('docum_value',true);
 
-            $data = array(
-                //'value_sign' => $this->input->post('value_sign', true),
-            );
+            if (is_numeric($docum_value))
+            {
+                if ($docum_value >= 0)
+                {
+                    $data = array(
+                        'nr_docum' => $this->input->post('nr_docum', true),
+                        'nr_mat' => $this->input->post('nr_mat', true),
+                        'docum_value' => $docum_value,
+                        //'value_sign' => $this->input->post('value_sign', true),
+                    );
+                    $this->Admin_model->create("DOCUM_ITEM", $data);
 
-            $this->Admin_model->create("DOCUM_ITEM", $data);
+                    $data = array(
+                        'nr_docum' => $this->input->post('nr_docum', true),
+                        'docum_type' => "Zm",
+                        'vend_name' => $this->input->post('vend_name', true),
+                        'docum_desc' => $this->input->post('docum_desc', true),
+                    );
+                    $this->Admin_model->create("DOCUM_HEAD", $data);
+
+                    $this->session->set_flashdata('alert', "Zamówienie zostało wysłane");
+                }
+                else
+                {
+                    $this->session->set_flashdata('alert', "Liczba zamawianego towaru musi być równa lub większa od zera!");
+                }
+            }
+            else
+            {
+                $this->session->set_flashdata('alert', "Liczba zamawianego towaru musi być wartością numeryczną!");
+            }
         }
 
-        $data['docum_type'] = $this->Admin_model->get("DOCUM_HEAD");
+        //$data['doctype'] = $this->Admin_model->get("DOCUM_HEAD");
         $data['zam'] = $this->Admin_model->get("VIEW_ORDER");
         $data['dost'] = $this->Admin_model->get('VEND');
         $data['validation'] = $this->session->flashdata('alert');
         $this->twig->display('admin/product/order_products',$data);
     }
-    public function order()
-    {
-        $data['indk_mwym'] = $this->Admin_model->get("MNAME");
-        $zam = $this->input->post('zam',true);
-        if (!empty($zam))
-        {
-            if (is_numeric($zam))
-            {
-                if (($zam >= 0))
-                {
-                    $data = array(
-                        //'nr_docum' => $?,
-                        //'nr_mat' => $?,
-                        'docum_value' => $zam,
-                    );
-                    $this->Admin_model->create("DOCUM_ITEM", $data);
-                    $this->session->set_flashdata('alert', "Zamówienie zostało wysłane");
-                }
-                else {
-                    $this->session->set_flashdata('alert', "Liczba zamawianego towaru musi być równa lub większa od zera!");
-                    $data['ilosc'] = $zam;
-                    $this->twig->display('admin/product/order_products_cd', $data);
-                }
-            } else {
-                $this->session->set_flashdata('alert', "Liczba do zamówienia musi być wartością numeryczną!");
-                $data['ilosc'] = $zam;
-                $this->twig->display('admin/product/order_products_cd', $data);
-            }
-        }
-        $data['validation'] = $this->session->flashdata('alert');
-        $this->twig->display('admin/product/order_products_cd', $data);
-    }
-    public function pz()
+    public function zm()
     {
         $total_rows = $this->Admin_model->num_rows("DOCUM_HEAD");
         $start_index = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0 ;
         $config['total_rows'] = $total_rows;
         $config['per_page'] = 4;
         $config['uri_segment'] = 4;
-        $config['base_url'] = base_url('admin/order/pz');
+        $config['base_url'] = base_url('admin/order/zm');
         $config['full_tag_open'] = '<ul class="pagination">';
         $config['full_tag_close'] = '</ul>';
         $config['first_link'] = 'First';
@@ -103,7 +91,7 @@ class Order extends Admin_Controller
         $this->pagination->initialize($config);
         $data['links'] = $this->pagination->create_links();
         $data['validation'] = $this->session->flashdata('alert');
-        $this->twig->display('site/product/list_pz',$data);
+        $this->twig->display('site/docs/list_order',$data);
     }
     public function show($id)
     {
@@ -111,12 +99,12 @@ class Order extends Admin_Controller
             $where = array('nr_docum' => $id);
             $data['docum_item'] = $this->Admin_model->get_single("DOCUM_ITEM", $where);
             $data['validation'] = $this->session->flashdata('alert');
-            $this->twig->display('admin/product/show', $data);
+            $this->twig->display('admin/order/show', $data);
         }
         else
         {
             $this->session->set_flashdata('alert', "Podany dokument nie istnieje !");
-            redirect(base_url("admin/product/pz"));
+            redirect(base_url("admin/order/view_order"));
         }
     }
     public function pdf()
